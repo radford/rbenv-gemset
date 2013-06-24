fa54d01f8b227bbcb6cc9a7d9ba1112bfb91d7c1
Jim Radford <radford@blackbean.org>
2013-06-24 15:42:20 -0700

    Simplify reash, fixing quoting and avoiding setup done in main rehash (cd / nullglob)

--- etc/rbenv.d/rehash/gemset.bash
+++ etc/rbenv.d/rehash/gemset.bash
@@ -1,10 +1,1 @@
-shopt -s nullglob
-OLDIFS="$IFS"
-IFS=$' \t\n'
-gemset_bin=(${RBENV_ROOT}/versions/*/gemsets/*/bin/*)
-IFS="$OLDIFS"
-shopt -s nullglob
-
-cd "$SHIM_PATH"
-make_shims ${gemset_bin[@]}
-cd "$CUR_PATH"
+make_shims ${RBENV_ROOT}/versions/*/gemsets/*/bin/*
